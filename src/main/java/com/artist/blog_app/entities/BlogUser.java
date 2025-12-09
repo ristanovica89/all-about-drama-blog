e14720ad04228935e600f20ce55bec06e988be5b
ristanovica89 <ristanovica89@gmail.com>
2025-12-09 14:51:08 +0100

    DB setup

---
+++ src/main/java/com/artist/blog_app/entities/BlogUser.java
@@ -0,0 +1,29 @@
+package com.artist.blog_app.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class BlogUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private Integer age;
+
+    private String gender;
+}
