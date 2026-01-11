77ae438eb9b15abc62bcfa77c1568e9a70e20a10
ristanovica89 <ristanovica89@gmail.com>
2026-01-11 23:47:00 +0100

    add: JwtAuthenticationEntryPoint

---
+++ src/main/java/com/artist/blog_app/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+package com.artist.blog_app.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull AuthenticationException authException) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        response.getWriter().write("""
+            {
+              "error": "Unauthorized",
+              "message": "Access denied"
+            }
+        """);
+    }
+}
